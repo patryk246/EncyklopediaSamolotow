c9e2dab7f8ab837b76d5b611d79174dd5c6cc20d
patryk246 <patryk.zamoscinski@gmail.com>
2019-12-30 15:25:15 +0100

    version with plane model

--- app/src/main/java/com/example/prototyp4/MainActivity.java
+++ app/src/main/java/com/example/prototyp4/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.prototyp4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.widget.Toast;
 
 import com.google.ar.core.Anchor;
 import com.google.ar.sceneform.AnchorNode;
@@ -13,24 +21,73 @@ import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.rendering.ShapeFactory;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.BaseArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ModelRenderable airplaneRenderable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (!checkIsSupportedDeviceOrFinish(this)) {
+            return;
+        }
         ArFragment arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+
+        ModelRenderable.builder()
+                // To load as an asset from the 'assets' folder ('src/main/assets/andy.sfb'):
+                .setSource(this, Uri.parse("PUSHILIN_Plane.sfb"))
+
+                // Instead, load as a resource from the 'res/raw' folder ('src/main/res/raw/andy.sfb'):
+                //.setSource(this, R.raw.andy)
+
+                .build()
+                .thenAccept(renderable -> airplaneRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Toast toast =
+                                    Toast.makeText(this, "Unable to load airplane renderable", Toast.LENGTH_LONG);
+                            toast.setGravity(Gravity.CENTER, 0, 0);
+                            toast.show();
+                            return null;
+                        });
+
         arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+
+            if (airplaneRenderable == null) {
+                return;
+            }
+            // Create the Anchor.
             Anchor anchor = hitResult.createAnchor();
-            MaterialFactory.makeOpaqueWithColor(this,new Color(android.graphics.Color.RED))
-                    .thenAccept(material -> {
-                        ModelRenderable renderable = ShapeFactory.makeSphere(0.25f,new Vector3(0f,0.25f,0.25f),material);
-                        AnchorNode anchorNode = new AnchorNode(anchor);
-                        anchorNode.setRenderable(renderable);
-                        arFragment.getArSceneView().getScene().addChild(anchorNode);
-                    });
-        });
+            AnchorNode anchorNode = new AnchorNode(anchor);
+            anchorNode.setParent(arFragment.getArSceneView().getScene());
+            // Create the transformable airplane and add it to the anchor.
+            TransformableNode airplane = new TransformableNode(arFragment.getTransformationSystem());
+            airplane.setParent(anchorNode);
+            airplane.setRenderable(airplaneRenderable);
+            airplane.select();
+            });
+    }
+
+    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
+            activity.finish();
+            return false;
+        }
+        String openGlVersionString =
+                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
+                        .getDeviceConfigurationInfo()
+                        .getGlEsVersion();
+        if (Double.parseDouble(openGlVersionString) < 3.0) {
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+                    .show();
+            activity.finish();
+            return false;
+        }
+        return true;
     }
 }
