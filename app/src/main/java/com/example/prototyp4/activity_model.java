cac752d3a0ccf1eac501e4fe764c05d1b45fadf3
Adrian Tylec <tylecadrian@gmail.com>
2020-03-14 12:25:44 +0100

    Dodanie activity_model, przełączenie uruchamiania na activity_model

---
+++ app/src/main/java/com/example/prototyp4/activity_model.java
@@ -0,0 +1,23 @@
+package com.example.prototyp4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class activity_model extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_model);
+
+
+    }
+    public void demoClicked(View v) {
+        startActivity(new Intent(activity_model.this, MainActivity.class));
+
+    }
+}
+
