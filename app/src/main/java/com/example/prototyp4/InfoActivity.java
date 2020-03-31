784659ab6c5f40ca2be2ef5449f85b3db21be4b4
Adrian Tylec <tylecadrian@gmail.com>
2020-03-31 15:29:06 +0200

    Dodanie informacji o samolotach (poprawienie commita 16)

--- app/src/main/java/com/example/prototyp4/InfoActivity.java
+++ app/src/main/java/com/example/prototyp4/InfoActivity.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class InfoActivity extends AppCompatActivity {
 
     DatabaseHelper DbHelper;
@@ -31,64 +34,76 @@ public class InfoActivity extends AppCompatActivity {
         if(name.equals("b787")){
             textView_name.setText("Boeing 787 Dreamliner");
             image.setImageResource(R.drawable.b787);
-            String text = "text";
-
-
-            text = DbHelper.getInfo(name);
-            textView_info.setText(text);
+            textView_info.setText(getInfo("b787.txt"));
         }
         if(name.equals("b29")) {
             textView_name.setText("Boeing b-29 Superfortress");
             image.setImageResource(R.drawable.b29);
-            //sql query
+            textView_info.setText(getInfo("b29.txt"));
         }
         if(name.equals("cc2")){
             textView_name.setText("Campini Caproni CC.2");
             image.setImageResource(R.drawable.cc2);
-            //sql query
+            textView_info.setText(getInfo("c22.txt"));
         }
         if(name.equals("ho229")){
             textView_name.setText("Horten HO 229");
             image.setImageResource(R.drawable.ho229);
-            //sql query
+            textView_info.setText(getInfo("ho229.txt"));
         }
         if(name.equals("f22")){
             textView_name.setText("Lockheed F-22 Raptor");
             image.setImageResource(R.drawable.f22);
-            //sql query
+            textView_info.setText(getInfo("f22.txt"));
         }
         if(name.equals("spad13")){
             textView_name.setText("Spad XIII");
             image.setImageResource(R.drawable.spad13);
-            //            //sql query
+            textView_info.setText(getInfo("spad13.txt"));
         }
         //helikoptery
         if(name.equals("uh1")){
             textView_name.setText("Bell UH-1 Iroquodis");
             image.setImageResource(R.drawable.uh1);
-            //sql query
+            textView_info.setText(getInfo("uh1.txt"));
         }
         if(name.equals("ah64")){
             textView_name.setText("Boeing AH-64 Apache");
             image.setImageResource(R.drawable.ah64);
-            //sql query
+            textView_info.setText(getInfo("ah64.txt"));
         }
         if(name.equals("wz10")){
             textView_name.setText("Changhe Wz-10");
             image.setImageResource(R.drawable.wz10);
-            //sql query
+            textView_info.setText(getInfo("wz10.txt"));
         }
         if(name.equals("md500")){
             textView_name.setText("Mcdonnell Douglas MD-500");
             image.setImageResource(R.drawable.md500);
-            //sql query
+            textView_info.setText(getInfo("md500.txt"));
         }
         if(name.equals("mi24")){
             textView_name.setText("MI-24");
             image.setImageResource(R.drawable.mi24);
-            //sql query
+            textView_info.setText(getInfo("mi24.txt"));
         }
 
+    }
+    public String getInfo(String name){
+        String text = "";
+        try {
+            InputStream stream = getAssets().open(name);
+            int size = stream.available();
+            byte[] buffer = new byte[size];
+            stream.read(buffer);
+            stream.close();
+            text = new String(buffer);
+
+        }catch (IOException e){
+            return e.toString();
+        }
+
+        return text;
     }
 
     public void showModelClicked(View view) {
